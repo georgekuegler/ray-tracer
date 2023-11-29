5804c8af39e4faf5d76ad95e8ce7a06aa17164fe
George Kuegler <georgekuegler@gmail.com>
2023-11-29 00:01:42 -0500

    completed section three

--- ray-tracer.cpp
+++ ray-tracer.cpp
@@ -2,10 +2,37 @@
 // begins and ends there.
 //
 
+/**
+
+ left off at -> 4.1
+ https://raytracing.github.io/books/RayTracingInOneWeekend.html#outputanimage/theppmimageformat
+
+*/
+
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int
 main()
 {
-  std::cout << "Hello World!\n";
+  int image_width = 256;
+  int image_height = 256;
+
+  // Render
+  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+  for (int j = 0; j < image_height; ++j) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
+              << std::flush;
+    for (int i = 0; i < image_width; ++i) {
+
+      auto pixel_color =
+        color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+
+      write_color(std::cout, pixel_color);
+    }
+  }
+  std::clog << "\rDone.                 \n";
 }
